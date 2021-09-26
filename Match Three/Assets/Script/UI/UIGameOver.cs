ba955aa971455bea2429f8cf3cb582422a5016a3
Dany Zaky <danyzakydany123@gmail.com>
2021-09-26 07:39:02 +0700

    mo nambahin kalo sempet

---
+++ Match Three/Assets/Script/UI/UIGameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGameOver : MonoBehaviour
+{
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void buttonRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
